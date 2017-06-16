a726c1bda3be854ab99ab662ebebfdeee111b778
hiroto <ichinose.higu@gmail.com>
2017-06-17 05:55:01 +0900

    fix indent

--- youtube_runner.rb
+++ youtube_runner.rb
@@ -18,39 +18,39 @@ module Tasks
       	def multi_channels()
       	    ids = ['CHANNE_ID', 'CHANNE_ID', 'CHANNE_ID']
             ids.each do |channel_id|
-          	    channel = Yt::Channel.new  id: channel_id
-          	    channel.videos.each do |video|
-					output_video(video)
+                channel = Yt::Channel.new  id: channel_id
+                channel.videos.each do |video|
+                    output_video(video)
                 end
             end
         end
         
         def recent_popular_list()
-			videos = Yt::Collections::Videos.new
-			videos.where(chart: 'mostPopular', region_code: 'JP').each do |video|
-				output_video(video)
-			end
-		end
+            videos = Yt::Collections::Videos.new
+            videos.where(chart: 'mostPopular', region_code: 'JP').each do |video|
+                output_video(video)
+            end
+	end
         
         private 
             def output_video(video)
                 p video.id
-				p video.channel_id
-				p video.title
-				p video.category_id
-				p video.thumbnail_url
-				p video.view_count
-				p video.like_count
-				p video.dislike_count
-				p video.tags
-				p video.published_at
-				if video.content_detail then
-					p video.duration
-				end
+                p video.channel_id
+                p video.title
+                p video.category_id
+                p video.thumbnail_url
+                p video.view_count
+                p video.like_count
+                p video.dislike_count
+                p video.tags
+                p video.published_at
+                if video.content_detail then
+                    p video.duration
+                end
 
-				if video.video_category then
-					p video.category_title
-				end
+                if video.video_category then
+                    p video.category_title
+                end
             end
     end
 end
