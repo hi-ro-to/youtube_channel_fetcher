8e0474554c9286c55016b29551527e3a0cdd0bf0
hiroto <ichinose.higu@gmail.com>
2017-06-17 05:47:22 +0900

    get popular list

--- youtube_runner.rb
+++ youtube_runner.rb
@@ -12,6 +12,7 @@ module Tasks
 
         def runTask
             multi_channels()
+            recent_popular_list()
         end
 
       	def multi_channels()
@@ -19,16 +20,38 @@ module Tasks
             ids.each do |channel_id|
           	    channel = Yt::Channel.new  id: channel_id
           	    channel.videos.each do |video|
-              		  p video.id
-                    p video.title
-                    p video.thumbnail_url
-                    p video.tags
-                    p video.view_count
-                    p video.like_count
-                    p video.dislike_count
+					output_video(video)
                 end
             end
         end
+        
+        def recent_popular_list()
+			videos = Yt::Collections::Videos.new
+			videos.where(chart: 'mostPopular', region_code: 'JP').each do |video|
+				output_video(video)
+			end
+		end
+        
+        private 
+            def output_video(video)
+                p video.id
+				p video.channel_id
+				p video.title
+				p video.category_id
+				p video.thumbnail_url
+				p video.view_count
+				p video.like_count
+				p video.dislike_count
+				p video.tags
+				p video.published_at
+				if video.content_detail then
+					p video.duration
+				end
+
+				if video.video_category then
+					p video.category_title
+				end
+            end
     end
 end
 
